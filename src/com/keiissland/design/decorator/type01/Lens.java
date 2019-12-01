667a454923761a1cc13dd83b34f7210bc9841ddd
Keiissland <1314..p&k>
2019-12-01 21:59:41 +0800

    增加必要的注释

--- src/com/keiissland/design/decorator/type01/Lens.java
+++ src/com/keiissland/design/decorator/type01/Lens.java
@@ -1,7 +1,13 @@
 package com.keiissland.design.decorator.type01;
 
+/**
+ * 镜头抽象类
+ */
 public abstract class Lens implements Photographable {
 
+    /**
+     * 持有被装饰类引用
+     */
     public Photographable photographable;
 
     public Lens(Photographable photographable) {
